4d2ca68e70b7168a3556c74e63a935b398b9b4f3
John-Rashta <john_rashta@outlook.com>
2024-07-15 20:08:06 +0100

    Finish menu script
    Finished the menu generation in javascript, doing some styling now,
    wont do much tho since exercise is mostly the get a bit familiar with
    webpack and splitting code up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "../assets/styles/styles.css";
 import Home from "./home.js";
 import About from "./about.js";
+import Menu from "./menu.js";
 
 
 Home();
@@ -8,5 +9,6 @@ Home();
 
 document.querySelector("#home").addEventListener("click", Home);
 document.querySelector("#about").addEventListener("click", About);
+document.querySelector("#menu").addEventListener("click", Menu);
 
 
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,10 @@
-
+import lasagnaImage from "../assets/images/lasagna.png";
+import pastaImage from "../assets/images/pasta.png";
+import pizzaImage from "../assets/images/pizza.png";
+import ribsImage from "../assets/images/ribs.png";
+import shawarmaImage from "../assets/images/shawarma.png";
+import soupImage from "../assets/images/soup.png";
+import tacoImage from "../assets/images/taco.png";
 
 
 
@@ -17,8 +23,70 @@ export default function loadMenu() {
     header.textContent = "Menu";
     const menuContainer = document.createElement("div");
     menuContainer.classList.toggle("menuGrid");
+    container.appendChild(header);
+    container.appendChild(menuContainer);
 
-    const itemNames = []
+    const itemsLibrary= [];
+
+    const itemNames = ["Lasagna",
+                       "Spaghetti Bolognese",
+                       "Pizza",
+                       "Ribs",
+                       "Shawarma",
+                       "Soup",
+                       "Tacos"
+
+    ];
+    
+    const itemDesc = ["Delicious lasagna made with the best sauce and cheese!",
+                      "Amazing pasta with meatballs and homemade sauce.",
+                      "Your choice of toppings pizza made on the spot!",
+                      "Quality meat ribs with a side of mashed potatoes",
+                      "Shawarma with your choice of meat.",
+                      "Soup of the day made with fresh ingredients",
+                      "Tacos with a selection of dipping sauces."
+    ];
+
+    const itemImg = [lasagnaImage,
+                     pastaImage,
+                     pizzaImage,
+                     ribsImage,
+                     shawarmaImage,
+                     soupImage,
+                     tacoImage
+    ];
+
+    const itemsLength = itemNames.length;
+
+    for (let i = 0; i < itemsLength; i++) {
+
+        const item = new menuItems(itemNames[i], itemDesc[i], itemImg[i]);
+        itemsLibrary.push(item);
+
+
+    }
+
+    itemsLibrary.forEach( (item) => {
+
+        const itemContainer = document.createElement("div");
+        const itemHead = document.createElement("h2");
+        const itemBody = document.createElement("div");
+        const itemDescription = document.createElement("div");
+        const itemImage = new Image();
+        itemImage.src = item.img;
+        itemHead.textContent = item.name;
+        itemDescription.textContent = item.desc;
+
+
+        itemBody.appendChild(itemDescription);
+        itemBody.appendChild(itemImage);
+        itemContainer.appendChild(itemHead);
+        itemContainer.appendChild(itemBody);
+        menuContainer.appendChild(itemContainer);
+
+
+
+    })
 
 
 }
